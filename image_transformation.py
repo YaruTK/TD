8c43689d2f8b4ad4e43b8b0192dfc2ee6e77147e
YaruTK <mcdonalddark@gmail.com>
2020-04-06 19:35:04 +0200

    Textures generation, auto-scaling pictures

--- image_transformation.py
+++ image_transformation.py
@@ -2,44 +2,76 @@ import numpy as np
 import cv2
 
 
-def order_points(pts):
-    rect = np.zeros((4,2), dtype = "float32")
-    s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
-
-    diff = np.diff(pts, axis = 1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
+def coord(image):
+    rect = np.zeros((4, 2), dtype="float32")
+    h, w, _ = image.shape
+    rect[0] = [0, 0]
+    rect[1] = [w - 1, 0]
+    rect[2] = [w - 1, h - 1]
+    rect[3] = [0, h - 1]
     return rect
 
-def four_point_transformation(image, pts):
-    rect = order_points(pts)
-    (tl, tr, br, bl) = rect
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
 
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
+def center(rect):
+    (x0, y0), (x1, y1), (x2, y2), (x3, y3) = rect
+    x_mid = (x0 * x1 * (y2 - y3) + x0 * x3 * (y1 - y2) + x1 * x2 * (y3 - y0) + x2 * x3 * (y0 - y1)) /\
+            ((x0 - x2) * (y1 - y3) + x1 * (y2 - y0) + x3 * (y0 - y2))
+    y_mid = (x0 * y1 * y2 - x0 * y2 * y3 - x1 * y0 * y3 + x1 * y2 * y3 + x2 * y0 * (y3 - y1) + x3 * y1 * (y0 - y2)) /\
+            ((x0 - x2) * (y1 - y3) + x1 * (y2 - y0) + x3 * (y0 - y2))
+    crd = np.array((x_mid, y_mid), dtype = "float32")
+    return crd
 
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight -1]], dtype = "float32")
-    M = cv2.getPerspectiveTransform(rect, dst)
+
+def distance(pt1, pt2):
+    x0, y0 = pt1
+    x1, y1 = pt2
+    square = (y1 - y0) ** 2 + (x1 - x0) ** 2
+    dist = np.sqrt(square)
+    return dist
+
+
+def comp(want, have):
+    border = ((min(want[0][0], want[3][0]), min(want[0][1], want[1][1])),
+              (max(want[1][0], want[2][0]), min(want[0][1], want[1][1])),
+              (max(want[1][0], want[2][0]), max(want[2][1], want[3][1])),
+              (min(want[0][0], want[3][0]), max(want[2][1], want[3][1])))
+
+    return border
+
+
+def transform(image, dest):
+    dst = np.array(dest, dtype="float32")
+    orig = coord(image)
+    border = comp(dst, orig)
+
+    width1 = distance(border[0], border[1])
+    width2 = distance(border[2], border[3])
+    maxWidth = max(int(width1), int(width2))
+
+    height1 = distance(border[0], border[3])
+    height2 = distance(border[1], border[2])
+    maxHeight = max(int(height1), int(height2))
+    M = cv2.getPerspectiveTransform(coord(image), dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
     return warped
 
 
-img = cv2.imread("test.png")
-pts = np.array(eval("(12,12), (240,0), (200, 200), (0, 240)"), dtype = "float32")
-warped = four_point_transformation(img, pts)
-cv2.imshow("original", img)
-cv2.imshow("Warped", warped)
-cv2.waitKey(0)
+def transpar(src):
+    tmp = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
+    b, g, r = cv2.split(src)
+    rgba = [b, g, r, alpha]
+    dst = cv2.merge(rgba, 4)
+    return dst
 
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+def fourChannels(img):
+    h, w, channels = img.shape
+    if channels < 4:
+        new_img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+        return new_img
+    return img
+
+
+
+
--- main_window.py
+++ main_window.py
@@ -1,4 +1,7 @@
 import pygame
+import numpy as np
+import image_transformation as it
+import cv2
 
 pygame.init()
 
@@ -12,8 +15,8 @@ black = (0, 0, 0)
 pink = (255, 200, 200)
 
 # int
-blockSize = 61  # size of a block in grid + 1
-perspective = 0.8  # 30% inclination
+blockSize = 127  # size of a block in grid + 1
+perspective = 0.87  # 30% inclination
 
 # str
 ver = '0.0.9'
@@ -75,52 +78,110 @@ class object2d(object):
             self.inclined = False
 
     def draw(self, clr, window):
-        pygame.draw.lines(window, clr, True,
+        pygame.draw.aalines(window, clr, True,
                           [(self.tlX, self.tlY), (self.trX, self.trY), (self.brX, self.brY), (self.blX, self.blY)])
+
+
+def plot(ar, color, window):
+    pygame.draw.aalines(window, color, True, ar)
 
 
 screen = pygame.display.set_mode((wind_width, wind_height))
 
 pygame.display.set_caption("Tower defense v." + ver)
 
-field = object2d(int(0.05 * wind_width), int(0.1 * wind_height), int(0.9 * wind_width), int(0.8 * wind_height))
+field = object2d(int(0.08 * wind_width), int(0.03 * wind_height), int(0.83 * wind_width), int(0.81 * wind_height))
+
+grid_height = 1 + int((field.height + 0.5 * blockSize - field.y - 1) / blockSize)
+grid_width = 1 + int((field.width + 0.5 * blockSize - field.x - 1) / blockSize)
+
+top_tiles = np.empty((grid_height*grid_width + 1, 4, 2), dtype = "float32")
+bot_tiles = np.empty((grid_height*grid_width + 1, 4, 2), dtype = "float32")
+
+plottop = np.zeros((grid_height, grid_width, 4), dtype=tuple)
+plotbot = np.zeros((grid_height, grid_width, 4), dtype=tuple)
 
 
-# def drawGrid(z):
-#     for y in range(field.y, field.height, blockSize):
-#         for x in range(field.x, field.width, blockSize):
-#             cell = object2d(x, y, blockSize - 1, blockSize - 1)
-#             for i in range(-5, 5, 1):  # draw 10 layers
-#                 cell.level = i * blockSize
-#                 cell.undo_inclination()
-#                 cell.do_inclination(z)
-#                 cell.draw(screen)
-#             cell.level = 0
-def drawGrid():
-    for y in range(field.y, field.height + blockSize - 1, blockSize):
-        for x in range(field.x, field.width + blockSize - 1, blockSize):
+def til(array, t, a):
+    tiles = np.zeros((4, 2), dtype = "float32")
+    tiles[0] = (float(a.tlX), float(a.tlY))
+    tiles[1] = (float(a.trX), float(a.trY))
+    tiles[2] = (float(a.brX), float(a.brY))
+    tiles[3] = (float(a.blX), float(a.blY))
+    array[t] = tiles
+
+
+def to_plot(a):
+    area = ((a.tlX, a.tlY), (a.trX, a.trY), (a.brX, a.brY), (a.blX, a.blY))
+    return area
+
+
+def calcGrid(f):
+    temp = 0
+    for i in range(0, grid_height):
+        y = f.y + i * blockSize
+        for j in range(0, grid_width):
+            x = f.x + j * blockSize
             cellbot = object2d(x, y, blockSize - 1, blockSize - 1)
             celltop = object2d(x, y, blockSize - 1, blockSize - 1)
-            cellbot.level = 0
-            celltop.level = 2 * blockSize
+            cellbot.level = 3 * blockSize
+            celltop.level = 0
             celltop.do_inclination(perspective)
             cellbot.do_inclination(perspective)
-            cellbot.draw(white, screen)
-            celltop.draw(red, screen)
+
+            plottop[i][j] = to_plot(celltop)
+            plotbot[i][j] = to_plot(cellbot)
+
+            til(top_tiles, temp, celltop)
+            til(bot_tiles, temp, cellbot)
+
+            temp += 1
+
+
+def drawGrid(layer, colour):
+    calcGrid(field)
+    for i in range(0, grid_height):
+        for j in range(0, grid_width):
+            plot(layer[i][j], colour, screen)
+
+
+def create(array):
+    calcGrid(field)
+    temp = array
+    for i in range(0, grid_width*grid_height):
+        erase_0 = min(temp[i][0][0], temp[i][3][0])
+        erase_1 = min(temp[i][0][1], temp[i][1][1])
+        for j in range(0, 4):
+            temp[i][j][0] -= erase_0
+            temp[i][j][1] -= erase_1
+            #print(str((temp[i][j][0], temp[i][j][1])))
+        picture = it.transpar(it.transform(cv2.imread('test.png'), temp[i]))
+        cv2.imwrite("resources/" + str(i) + ".png", picture)
+
+
+def draw_tiles(array):
+    calcGrid(field)
+    for i in range(0, grid_width*grid_height):
+        x = int(min(array[i][0][0], array[i][3][0]))
+        y = int(min(array[i][0][1], array[i][1][1]))
+        temp = pygame.image.load('resources/' + str(i) + '.png')
+        screen.blit(temp, (x, y))
 
 
 def redrawGameWindow():
-    screen.fill(darkBlue)
-    drawGrid()
+    #screen.fill(darkBlue)
+    screen.blit(pygame.image.load('background.jpg'), (0, 0))
+    drawGrid(plotbot, white)
+    draw_tiles(top_tiles)
+    drawGrid(plottop, black)
     pygame.display.update()
 
+
+create(top_tiles)
 
 # main loop
 
 run = True
-a = 0
-b = 0
-c = 0
 while run:
     fps.tick(60)
 
@@ -128,5 +189,4 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    redrawGameWindow()
-    # for real game
+    redrawGameWindow()  # for real game
